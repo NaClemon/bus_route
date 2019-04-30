2acd0d04c9eb6c96ea3c1529804bf9b4c1b40665
bloquer <995skh@gmail.com>
2019-04-30 19:49:02 +0900

    read file function
    파일 읽기 함수 구현 완료

--- BusRoute/BusRoute/Main.c
+++ BusRoute/BusRoute/Main.c
@@ -2,11 +2,30 @@
 #include <stdlib.h>
 #include <Windows.h>
 
-struct bus {
-	char* bus_name = (char*)malloc(sizeof(char) * 100);
+/* 버스 정거장 구조체 */
+typedef struct bus {
+	char station[255];
 	double longitude;
 	double latitude;
-}BUS;
+}Bus;
+
+/* 주요 장소 구조체 */
+typedef struct place {
+	char name[50];
+	double longitude;
+	double latitude;
+}Place;
+
+/* 버스 개수 만큼 전역 변수 설정 */
+Bus* A;
+
+/* 구현 완료 함수 */
+
+void Read_File(char*, Bus*);		// 파일 읽기 함수
+void gotoxy(int x, int y);
+
+
+/* 구현 미완료 함수 */
 
 void Mainmenu();		// 메인 화면 출력 함수
 void Menu_station();	// 버스 목록 출력 함수
@@ -15,16 +34,8 @@ void Menu_place();		// 주요 장소 출력 함수
 void Place_Bus();		// 주요 장소 근처 정류장 출력 함수
 void Result();			// 결과 화면 출력 함수
 void Detail_result();	// 결과 자세히 보기 함수
-void Read_File(char*);		// 파일 읽기 함수
 void Click();
 
-struct place {
-	char name[50];
-	double longitude;
-	double latitude;
-};
-
-void gotoxy(int x, int y);
 void draw_square(int bus_num, int x);
 void draw_line();
 void bus_line(int bus_num);
@@ -32,13 +43,17 @@ void Menu_place();
 
 int main()
 {
+	int num;
+	A = (Bus*)malloc(sizeof(Bus) * 100);
+	Read_File("512.csv", A);
+
 	int bus_num;
 	draw_square(823, 0);
 	gotoxy(10, 1);
 	draw_square(717, 10);
 	gotoxy(1, 10);
 	printf("버스 번호를 입력하시오 : ");
-	scanf_s("%d", &bus_num, sizeof(int));
+	scanf_s("%d", &bus_num);
 	system("cls");
 
 	draw_square(bus_num, 0);
@@ -46,7 +61,7 @@ int main()
 	draw_line();
 
 
-
+	free(A);
 	return 0;
 }
 
@@ -132,16 +147,50 @@ void bus_line(int bus_num)
 	printf("%c%c", a, b[5]);
 }
 
-void Read_File(char* name)
+/// <summary>
+/// 파일 읽기 함수
+/// </summary>
+/// <param name="name">읽어올 파일 이름</param>
+/// <param name="bus_num">버스 정보를 저장할 구조체</param>
+void Read_File(char* name, Bus* bus_num)
 {
-	FILE* fl;
+	FILE* bus_inform;
+	char str[255];
+	char* temp;
+	char* temp_station, * temp_longitude, * temp_latitude;
+	int i = 0;
+
+	fopen_s(&bus_inform, name, "rb");
+
+	if (bus_inform != NULL)
+	{
+		while (!feof(bus_inform))
+		{
+			temp = NULL;
+			temp = fgets(str, sizeof(str), bus_inform);
+
+			if (temp == NULL)
+				break;
+
+			temp_station = strtok_s(temp, ",\r\n", &temp);
+			temp_longitude = strtok_s(temp, ",\r\n", &temp);
+			temp_latitude = strtok_s(temp, ",\r\n", &temp);
+
+			strcpy_s(bus_num[i].station, sizeof(bus_num[i].station), temp_station);
+			bus_num[i].longitude = atof(temp_longitude);
+			bus_num[i].latitude = atof(temp_latitude);
+
+			i++;
+		}
+		fclose(bus_inform);
+	}
 }
+
 void Menu_place()
 {
 	int item = 0;
-	place* Place;
-	place start, finish;
-	int num; //y좌표
+	Place* place;
+	int num = 0; //y좌표
 	//파일 불러오기
 	//불러온 파일 이용하여 place 동적할당 및 내용저장 \
 	  item 수 저장 후 파일 클로즈
